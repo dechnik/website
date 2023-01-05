33fa7758e9fc6dc897dd0d3ecadc71338cae9323
Lukasz Dechnik <lukasz@dechnik.net>
2023-01-05 09:03:14 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+gem "jekyll", "~>4.2"
+
+group :jekyll_plugins do
+  gem "jekyll-relative-links", git: 'https://github.com/misterio77/jekyll-relative-links', branch: 'main'
+  gem "jekyll-extlinks", git: 'https://github.com/misterio77/jekyll-extlinks'
+end
---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = nixpkgs.legacyPackages;
+    in
+    rec {
+      packages = forAllSystems (system: rec {
+        main = pkgsFor.${system}.callPackage ./. { };
+        default = main;
+      });
+
+      hydraJobs = {
+        x86_64-linux.main = packages.x86_64-linux.main;
+      };
+    };
+}
