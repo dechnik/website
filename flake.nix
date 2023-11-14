370d6279ed2c189f2bf161ce399caf22641e49d6
lukasz Dechnik <lukasz@dechnik.net>
2023-11-14 11:40:58 +0100

    nix flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My personal website and blog";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
